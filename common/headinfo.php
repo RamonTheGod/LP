dae466d82fc065bf7af7db86e65918ec5fe06b87
RamonTheGod <52341462+RamonTheGod@users.noreply.github.com>
2020-02-13 17:02:37 +0800

    Icon script update

--- common/headinfo.php
+++ common/headinfo.php
@@ -10,7 +10,8 @@
 <meta name="googlebot" content="all">
 <meta name="verify-v1" content="o7DZr4liQyQ4OhbHTTez036lykxg2xn1sRx6YUHsT3w=" />
 <!-- <link rel="Shortcut Icon" href="/launchpages.ico"> -->
-<link rel="shortcut icon" type="image/x-icon" href="favicon/favicon.ico" />
+<!-- <link rel="shortcut icon" type="image/x-icon" href="favicon/favicon.ico" /> -->
+<link rel="shortcut icon" type="image/x-icon" href="favicon/favicon.png" />
 <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
