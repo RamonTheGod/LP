854a1c675691f1baa8fdbb3746d5a105850620dd
RamonTheGod <52341462+RamonTheGod@users.noreply.github.com>
2020-02-13 16:34:08 +0800

    Favicon update - script

--- common/headinfo.php
+++ common/headinfo.php
@@ -9,7 +9,12 @@
 <meta name="keywords" content="LaunchPages">
 <meta name="googlebot" content="all">
 <meta name="verify-v1" content="o7DZr4liQyQ4OhbHTTez036lykxg2xn1sRx6YUHsT3w=" />
-<link rel="Shortcut Icon" href="/launchpages.ico">
+<!-- <link rel="Shortcut Icon" href="/launchpages.ico"> -->
+<link rel="shortcut icon" type="image/x-icon" href="favicon/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+<link rel="manifest" href="favicon/site.webmanifest">
 <style type="text/css">
 a {
 	color: <?php echo $fcolor ?>;
